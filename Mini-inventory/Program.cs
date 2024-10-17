9396d4a282489be2730b712f20ce60b0a238cafe
ali aldalati <ali.aldalati@elev.ga.ntig.se>
2024-10-17 16:04:04 +0200

    sdfsdf

--- Mini-inventory/Program.cs
+++ Mini-inventory/Program.cs
@@ -2,5 +2,24 @@
 
 c.Backpack.Display();
 
+Armor hjälm = new() {Name = "dragonhelmet"};
+Console.WriteLine($"Det finns en ny {hjälm.Name}");
+Console.WriteLine("Vill du plocka up den");
+
+Console.ReadLine();
+
+string Answer = Console.ReadLine();
+
+if ( Answer == "ja")
+
+{
+    c.Backpack.Items.Add(hjälm);
+    Console.WriteLine($"{hjälm.Name} har lagts till i din backpack.");
+}
+
+else if (Answer =="nej")
+{
+    Console.WriteLine($"du har missat {hjälm.Name}");
+}
 
 Console.ReadLine();
